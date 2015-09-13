d746d11ce2b05e64bf297070e564df7946f1f63a
Allison Logan <ali.cand@yahoo.com>
2015-09-13 12:14:44 -0400

    updates pages

--- page.php
+++ page.php
@@ -1,20 +1,18 @@
 <?php get_header(); ?>
-    <div class="row">
-<!-- BEGIN PAGE PHP -->
-        <div class="nine columns">
-            <?php if (have_posts()) : 
-                /* OUR DATA CONTEXT IS DEFINED  */
-                while (have_posts()) : the_post(); ?> 
-                    <h3><?php the_title(); ?></h3>
-                    <?php the_content();
-                endwhile;
-            endif; ?>
+    <div class="container othercontent">
+        <div class="row">
+    <!-- BEGIN PAGE PHP -->
+            <div class="twelve columns">
+                <?php if (have_posts()) : 
+                    /* OUR DATA CONTEXT IS DEFINED  */
+                    while (have_posts()) : the_post(); ?> 
+                        <h3><?php the_title(); ?></h3>
+                        <?php the_content();
+                        comments_template( $file, $separate_comments );
+                    endwhile;
+                endif; ?>
+            </div>
+    <!-- END PAGE PHP -->
         </div>
-<!-- END PAGE PHP -->
-<!-- BEGIN SIDEBAR AREA -->
-        <div class="three columns">
-            <?php get_sidebar(); ?>
-        </div>
-<!-- END SIDEBAR AREA -->
     </div>
 <?php get_footer(); ?>
